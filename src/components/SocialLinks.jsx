367181d8a39a7b05e817a8daccf8eca4bc9be8f3
ATG-5222 <A01275222@tec.mx>
2023-09-19 11:24:25 -0600

    Ajustar enlaces en SocialLinks

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,7 +12,7 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={30} />
         </>
       ),
-      href: "linkedin.com/in/aldo-tena-garcía-709133281",
+      href: "https://www.linkedin.com/in/aldo-tena-garcía-709133281/",
       style: "rounded-tr-md",
     },
     {
@@ -31,7 +31,7 @@ const SocialLinks = () => {
           Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "atg.00019@gmail.com",
+      href: "mailto:atg.00019@gmail.com",
     },
     // {
     //   id: 4,
